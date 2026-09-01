9b56f81697819753ad5f432ec569dacac2faa5d1
FeliOjedaRuiz <feliojedaruiz@gmail.com>
2026-09-01 14:09:35 +0200

    Final

---
+++ dist/_astro/config.BomGm1kI.js
@@ -0,0 +1,9 @@
+import{r as l}from"./index.CVf8TyFT.js";var _={exports:{}},s={};/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var u=l,c=Symbol.for("react.element"),f=Symbol.for("react.fragment"),i=Object.prototype.hasOwnProperty,m=u.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,O={key:!0,ref:!0,__self:!0,__source:!0};function E(t,r,a){var e,o={},n=null,p=null;a!==void 0&&(n=""+a),r.key!==void 0&&(n=""+r.key),r.ref!==void 0&&(p=r.ref);for(e in r)i.call(r,e)&&!O.hasOwnProperty(e)&&(o[e]=r[e]);if(t&&t.defaultProps)for(e in r=t.defaultProps,r)o[e]===void 0&&(o[e]=r[e]);return{$$typeof:c,type:t,key:n,ref:p,props:o,_owner:m.current}}s.Fragment=f;s.jsx=E;s.jsxs=E;_.exports=s;var y=_.exports;const R=new Date("2026-09-19T17:30:00+02:00"),T="https://www.google.com/maps/place/D%E2%80%99locosparty+Parque+de+bolas+en+Granada/@37.2222644,-3.6960917,17z",x="http://localhost:4321",N=2,v="Europe/Madrid";export{R as E,T as G,x as I,N as a,v as b,y as j};
