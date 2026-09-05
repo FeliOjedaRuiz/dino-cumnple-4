d909def99a74e68f15c9fbcaf6f69a86179ffcdd
FeliOjedaRuiz <feliojedaruiz@gmail.com>
2026-09-06 00:18:23 +0200

    fix: add .gitignore and remove tracked dist/; fix Dino.png casing

--- dist/_astro/config.BW-_8cPO.js
+++
@@ -1,9 +0,0 @@
-import{r as E}from"./index.CVf8TyFT.js";var _={exports:{}},s={};/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var f=E,u=Symbol.for("react.element"),c=Symbol.for("react.fragment"),l=Object.prototype.hasOwnProperty,m=f.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,O={key:!0,ref:!0,__self:!0,__source:!0};function i(t,r,p){var e,o={},n=null,a=null;p!==void 0&&(n=""+p),r.key!==void 0&&(n=""+r.key),r.ref!==void 0&&(a=r.ref);for(e in r)l.call(r,e)&&!O.hasOwnProperty(e)&&(o[e]=r[e]);if(t&&t.defaultProps)for(e in r=t.defaultProps,r)o[e]===void 0&&(o[e]=r[e]);return{$$typeof:u,type:t,key:n,ref:a,props:o,_owner:m.current}}s.Fragment=c;s.jsx=i;s.jsxs=i;_.exports=s;var d=_.exports;const R=new Date("2026-09-19T17:30:00+02:00"),T="https://www.google.com/maps?q=D%27Locosparty+Granada",x="https://invitacion-dino.netlify.app",v=2,N="Europe/Madrid";export{R as E,T as G,x as I,v as a,N as b,d as j};
